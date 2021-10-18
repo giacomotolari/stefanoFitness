d5dd36a9b6191f3efafc63ac131fcd6fb49e2d77
giacomotolari <tolarigiacomo@gmail.com>
2021-10-18 15:58:48 +0200

    add about text

--- src/components/main/about/About.js
+++ src/components/main/about/About.js
@@ -1,50 +1,34 @@
 import './about.scss';
 
-
 function About() {
   return (
     <div className='About'>
       <h1>STEFANO MANCA</h1>
       <section className='sectionAbout'>
         <p className='aboutText'>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          deleniti libero itaque quia dolore dolor distinctio molestias aut
-          minus doloremque dignissimos reprehenderit temporibus rem hic, enim
-          accusamus aspernatur. Fugiat, dolore! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Eveniet, deleniti libero itaque quia
-          dolore dolor distinctio molestias aut minus doloremque dignissimos
-          reprehenderit temporibus rem hic, enim accusamus aspernatur. Fugiat,
-          dolore!
+          Mi chiamo Stefano Manca e lavoro come personal trainer, insegnante di
+          Calisthenics e massaggiatore.
           <br />
           <br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          deleniti libero itaque quia dolore dolor distinctio molestias aut
-          minus doloremque dignissimos reprehenderit temporibus rem hic, enim
-          accusamus aspernatur. Fugiat, dolore! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Eveniet, deleniti libero itaque quia
-          dolore dolor distinctio molestias aut minus doloremque dignissimos
-          reprehenderit temporibus rem hic, enim accusamus aspernatur. Fugiat,
-          dolore!
-          <br /><br />
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          deleniti libero itaque quia dolore dolor distinctio molestias aut
-          minus doloremque dignissimos reprehenderit temporibus rem hic, enim
-          accusamus aspernatur. Fugiat, dolore! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Eveniet, deleniti libero itaque quia
-          dolore dolor distinctio molestias aut minus doloremque dignissimos
-          reprehenderit temporibus rem hic, enim accusamus aspernatur. Fugiat,
-          dolore! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet,
-          deleniti libero itaque quia dolore dolor distinctio molestias aut
-          minus doloremque dignissimos reprehenderit temporibus rem hic, enim
-          accusamus aspernatur. Fugiat, dolore! Lorem ipsum dolor sit amet,
-          consectetur adipisicing elit. Eveniet, deleniti libero itaque quia
-          dolore dolor distinctio molestias aut minus doloremque dignissimos
-          reprehenderit temporibus rem hic, enim accusamus aspernatur. Fugiat,
-          dolore!
+          Sono esperto del movimento, della cura e benessere del corpo: il mio
+          interesse verso queste discipline mi ha spinto nello specializzarmi in
+          diversi settori.
+          <br />
+          Nel 2014 mi sono laureato in “Scienze Motorie” presso l'Università di
+          Cagliari e nel 2019 ho conseguito la specializzazione in “Gestione
+          delle attività motorie preventive e adattate” all'Università di San
+          Raffaele di Roma. Ho preparato atleti non professionisti di calcio e
+          basket e insegnato ginnastica dolce agli adulti.
+          <br />
+          Dal 2017 sono istruttore di “Calisthenics”, ginnastica a corpo libero
+          e da diversi anni sono insegnante di espressione motoria e anatomia in
+          una scuola superiore paritaria. Grazie al diploma di Operatore del
+          massaggio, ottenuto presso la scuola “Sardegna massaggi” di Iglesias
+          nel 2016, lavoro come massaggiatore sia privatamente che presso alcune
+          strutture locali.
         </p>
         <div className='aboutFoto'></div>
       </section>
-     
     </div>
   );
 }
